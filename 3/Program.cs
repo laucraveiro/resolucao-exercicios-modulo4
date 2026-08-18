09ec4cffbd1455f43478265c7fbe57bdf0554e29
laucraveiro <lcrav135@gmail.com>
2026-08-18 19:49:55 -0300

    Add files via upload

---
+++ 3/Program.cs
@@ -0,0 +1,22 @@
+﻿int numero1;
+while (true)
+{
+    Console.WriteLine("Digite um número inteiro: ");
+    var valorDigitado1 = Console.ReadLine();
+
+    if (int.TryParse(valorDigitado1, out numero1))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Digite um número inteiro válido.");
+    }
+}
+
+int antecessor = numero1 - 1;
+int sucessor = numero1 + 1;
+
+Console.WriteLine($"Antecessor: {antecessor}");
+Console.WriteLine($"Número informado: {numero1}");
+Console.WriteLine($"Sucessor: {sucessor}");
