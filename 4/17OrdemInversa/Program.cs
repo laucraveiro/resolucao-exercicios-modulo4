dfd566cb7c237374fa4d71ab5a22eb30bd642261
laucraveiro <lcrav135@gmail.com>
2026-08-18 20:01:45 -0300

    Add files via upload

---
+++ 4/17OrdemInversa/Program.cs
@@ -0,0 +1,13 @@
+﻿double[] numeros = new double[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Digite o {i + 1}º número: ");
+    numeros[i] = double.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Números na ordem inversa:");
+for (int i = numeros.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(numeros[i]);
+}
