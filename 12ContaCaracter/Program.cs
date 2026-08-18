d7fa23e7c1c49b5e060c8bc7740b1aa3f77bb83a
laucraveiro <lcrav135@gmail.com>
2026-08-18 20:02:55 -0300

    Add files via upload

---
+++ 12ContaCaracter/Program.cs
@@ -0,0 +1,12 @@
+﻿string nome = "";
+
+Console.WriteLine("Insira um nome: ");
+nome = Console.ReadLine();
+
+int soma = 0;
+char[] letras = nome.ToCharArray();
+foreach(char c in nome)
+{
+    soma++;
+}
+Console.WriteLine($"A soma dos caracteres do nome {nome} é: {soma}");
