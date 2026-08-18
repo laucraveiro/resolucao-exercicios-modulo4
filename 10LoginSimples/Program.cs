73e46c34dc3be639e65bdb26d0f982d9676f8c95
laucraveiro <lcrav135@gmail.com>
2026-08-18 20:00:57 -0300

    Add files via upload

---
+++ 10LoginSimples/Program.cs
@@ -0,0 +1,10 @@
+﻿string senhaCorreta = "123";
+string senha = "";
+do
+{
+    Console.Clear();
+    Console.Write("Usuário: ");
+    string usuario = Console.ReadLine();
+    Console.Write("Senha: ");
+    senha = Console.ReadLine();
+} while (senha != senhaCorreta);
