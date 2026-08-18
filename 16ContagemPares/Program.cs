2a3eff18b832507187e0d5cba8c32dfed4fec6c8
laucraveiro <lcrav135@gmail.com>
2026-08-18 20:04:41 -0300

    Add files via upload

---
+++ 16ContagemPares/Program.cs
@@ -0,0 +1,14 @@
+﻿int quantidadePares = 0;
+
+for (int i = 1; i <= 20; i++)
+{
+    Console.WriteLine($"Digite o {i}º número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        quantidadePares++;
+    }
+}
+
+Console.WriteLine($"Quantidade de números pares: {quantidadePares}");
