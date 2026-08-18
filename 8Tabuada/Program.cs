73f32cc8db99cbd15fc65326f802c99752544d7e
laucraveiro <lcrav135@gmail.com>
2026-08-18 19:59:17 -0300

    Add files via upload

---
+++ 8Tabuada/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite um número: ");
+
+int numero = int.Parse(Console.ReadLine());
+
+for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+{
+    int resultado = numero * multiplicador;
+    Console.WriteLine($"{numero} x {multiplicador} = {resultado}");
+}
