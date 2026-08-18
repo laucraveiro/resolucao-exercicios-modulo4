288685f6ffefd2ecd765bf2a2d266df5b3b13b0e
laucraveiro <lcrav135@gmail.com>
2026-08-18 20:01:57 -0300

    Add files via upload

---
+++ 11SomaAte0/Program.cs
@@ -0,0 +1,15 @@
+﻿int numero = 1;
+int soma = 0;
+
+while (numero != 0)
+{
+    Console.Write("Insira um número inteiro: ");
+    numero = int.Parse(Console.ReadLine());
+    
+    if (numero != 0)
+    {
+        soma += numero;
+    }
+}
+
+Console.WriteLine($"Soma dos valores digitados: {soma}"); 
