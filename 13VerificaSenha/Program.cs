f19d5d7737ff944ee27cf5fe7da3a281af0cfff0
laucraveiro <lcrav135@gmail.com>
2026-08-18 20:03:23 -0300

    Add files via upload

---
+++ 13VerificaSenha/Program.cs
@@ -0,0 +1,20 @@
+﻿string senha = "";
+int contador = 0;
+
+do
+{
+    Console.WriteLine("Insira uma senha: ");
+    senha = Console.ReadLine();
+    contador = 0;
+
+    foreach (char c in senha)
+    {
+        contador++;
+    }
+    if (contador < 8)
+    {
+        Console.WriteLine($"A senha deve ter pelo menos 8 caracteres");
+    }
+} while(contador < 8);
+
+Console.WriteLine($"Senha aceita, possui {contador} caracteres");
