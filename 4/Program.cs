2abe3c820eac877f5d2a2762114c8f294ff920ad
laucraveiro <lcrav135@gmail.com>
2026-08-18 20:08:54 -0300

    Delete 4 directory

--- 4/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Digite o valor em reais: ");
-double reais = double.Parse(Console.ReadLine());
-
-Console.WriteLine("Digite o valor da cotação em dolár: ");
-double cotacaoDolar = double.Parse(Console.ReadLine());
-
-double dolar = reais / cotacaoDolar;
-
-Console.WriteLine($"R$ {reais:F2} valem $ {dolar:F2}");
