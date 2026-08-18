b8af7fe2a163bbf0220169419864283f45fa06e8
laucraveiro <lcrav135@gmail.com>
2026-08-18 20:06:45 -0300

    Add files via upload

---
+++ 3AreaRetangulo/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Vamos calcular a área e o perímetro de um retângulo!");
+
+Console.WriteLine("Digite a base do retângulo: ");
+double baseRetangulo = double.Parse(Console.ReadLine());
+            
+Console.WriteLine("Digite a altura do retângulo: ");
+double alturaRetangulo = double.Parse(Console.ReadLine());
+
+double area = baseRetangulo * alturaRetangulo;
+double perimetro = 2 * (baseRetangulo + alturaRetangulo);
+
+Console.WriteLine($"A área do retângulo é: {area}");
+Console.WriteLine($"O perímetro do retângulo é: {perimetro}");
