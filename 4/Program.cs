e17f82ceec9952a900c533f0656867ca585e269a
laucraveiro <lcrav135@gmail.com>
2026-08-18 19:52:47 -0300

    Add files via upload

---
+++ 4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite o valor em reais: ");
+double reais = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor da cotação em dolár: ");
+double cotacaoDolar = double.Parse(Console.ReadLine());
+
+double dolar = reais / cotacaoDolar;
+
+Console.WriteLine($"R$ {reais:F2} valem $ {dolar:F2}");
